e837284de597b2a4cefd2905d8aee51ec6d044d1
borninachenil <82093897+borninachenil@users.noreply.github.com>
2024-04-04 16:12:05 +0200

    Update template.py

--- template.py
+++ template.py
@@ -15,13 +15,10 @@ def charger_paires(relations):
                     terme1 = elements[1].strip()
                     paires[(terme1, elements[2].strip())] = True
     return paires
-
-
 def term_exists_in_phrase(term, mots):
     term_words = term.split()
     if not term_words:
         return -1 
-    
     term_length = len(term_words)
     
     for i in range(len(mots) - term_length + 1):
@@ -33,8 +30,7 @@ def term_exists_in_phrase(term, mots):
                     break
             if match:
                 return i  
-    return -1  #
-
+    return -1
 def charger_resultats_existants(fichier_template):
     resultats_existant = {}
     try:
@@ -47,11 +43,9 @@ def charger_resultats_existants(fichier_template):
     except FileNotFoundError:
         pass
     return resultats_existant
-
 def extraire_mots_intermediaires(json_filename, relations_filename, paires, resultats_existant):
     with open(json_filename, 'r', encoding='utf-8', errors='ignore') as f:
         data = json.load(f)
-
     nom_fichier_relations = relations_filename.split('/')[-1]
     for phrase in data:
         mots = [mot['mot'] for mot in phrase]
@@ -67,31 +61,22 @@ def extraire_mots_intermediaires(json_filename, relations_filename, paires, resu
                     mots_intermediaires = " ".join(mots[start_index2:end_index1])
                 if (abs(index1 - index2) <= 7 or abs(end_index1 - start_index2) <= 7) and mots_intermediaires.strip(): 
                     cle = (mots_intermediaires, nom_fichier_relations)
-                    resultats_existant[cle] = resultats_existant.get(cle, 0) + 1
-                    
+                    resultats_existant[cle] = resultats_existant.get(cle, 0) + 1    
     return resultats_existant
-
-
-
-
 def enregistrer_resultats(resultats_existant, fichier_template):
     lignes = [f"{mots_intermediaires};{relation.split('\\')[-1]};{occurrences}\n" for ((mots_intermediaires, relation), occurrences) in resultats_existant.items()]
     with open(fichier_template, 'w', encoding='utf-8') as f:
         f.writelines(lignes)
-
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Usage: python script.py relations json_file")
         sys.exit(1)
-    
     relations_filename = sys.argv[1]
     json_filename = sys.argv[2]
     fichier_template = "template.txt"
-    
     paires = charger_paires(relations_filename)
     resultats_existant = charger_resultats_existants(fichier_template)
     resultats_mis_a_jour = extraire_mots_intermediaires(json_filename, relations_filename, paires, resultats_existant)
-    
     enregistrer_resultats(resultats_mis_a_jour, fichier_template)
 
     print("Traitement terminé.")
